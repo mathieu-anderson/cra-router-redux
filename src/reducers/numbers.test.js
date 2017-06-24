c4be434a131383d25ab1deddbab82e77833d1900
MathieuA <mathieu.hj.anderson@gmail.com>
2017-06-24 11:39:29 +0200

    Successful reducer test

--- src/reducers/numbers.test.js
+++ src/reducers/numbers.test.js
@@ -3,16 +3,14 @@ import numbers from "./numbers";
 const initialState = [1, 2, 3];
 
 it("should return the initial state", () => {
-  expect(numbers(initialState, { type: "ADD_NUMBER" }).toEqual(initialState));
+  expect(numbers(initialState, {})).toEqual(initialState);
 });
 
 it("should add the specified number to the numbers array", () => {
-  expect(
-    numbers(initialState, { type: "ADD_NUMBER", number: 1 }).toEqual([
-      1,
-      2,
-      3,
-      1
-    ])
-  );
+  expect(numbers(initialState, { type: "ADD_NUMBER", number: 1 })).toEqual([
+    1,
+    2,
+    3,
+    1
+  ]);
 });
