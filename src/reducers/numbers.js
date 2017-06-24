32ae750af9c790986d4d7c804185206c02faebf1
MathieuA <mathieu.hj.anderson@gmail.com>
2017-06-24 15:10:33 +0200

    Change commonjs export to ES6 in numbers reducer

--- src/reducers/numbers.js
+++ src/reducers/numbers.js
@@ -1,4 +1,4 @@
-module.exports = function(state = [], action) {
+export default function numbers(state = [], action) {
   switch (action.type) {
     case "ADD_NUMBER":
       // with concat method : return state.concat([action.number]);
@@ -7,4 +7,4 @@ module.exports = function(state = [], action) {
     default:
       return state;
   }
-};
+}
